896cbb90ba2221bfa35a678d259615d66a932868
AleexPoorto <125435099+AleexPoorto@users.noreply.github.com>
2023-04-24 10:35:42 -0300

    Add files via upload

---
+++ Menor de 3.py
@@ -0,0 +1,13 @@
+a = int(input("primeiro valor "))
+b = int(input("segundo valor "))
+c = int(input("terceiro valor "))
+
+if a < b and a < c:
+    menor = a
+
+elif b < c:
+    menor = b
+else:
+    menor = c
+
+print(f"menor = {menor}!")
